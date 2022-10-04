e71d4797849711d5246ab479dee52c45a883165f
rali <rali@hubspot.com>
2022-10-04 10:10:11 -0400

    Initial Commit

---
+++ Shared/FEEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FEEDApp.swift
+//  Shared
+//
+//  Created by Rachel Li on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FEEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
